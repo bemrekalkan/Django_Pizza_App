ea50e4c563bd089dc3869b37498f20ec728c8399
bemrekalkan <bemre.kalkan@gmail.com>
2022-09-02 21:18:00 +0300

    models

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+class Size(models.Model):
+    SIZE = [
+        ("1" , "small"),
+        ("2" , "medium"),
+        ("3" , "large"),
+    ]
+    size = models.CharField(max_length=1, choices=SIZE)
 
-# Create your models here.
+class Pizza(models.Model):
+    topping1 = models.CharField("Topping_1", max_length=50)
+    topping2 = models.CharField("Topping_2", max_length=50)
+    size = models.OneToOneField(Size, on_delete=models.CASCADE)
