cb290220b75efd6e467e8b3c7572c3ac07d5e025
bemrekalkan <bemre.kalkan@gmail.com>
2022-09-03 15:27:29 +0300

    .

--- sunsgarden/settings.py
+++ sunsgarden/settings.py
@@ -38,11 +38,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     # 3rd part apps
     'crispy_forms',
-    
+
     # my_apps
-    "pizza",
+    'pizza',
 ]
 
 MIDDLEWARE = [
