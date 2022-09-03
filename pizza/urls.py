146309377c1ff12239d098b9e3ebdc80ba5e7095
bemrekalkan <bemre.kalkan@gmail.com>
2022-09-03 15:10:16 +0300

    .

--- pizza/urls.py
+++ pizza/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+
+from pizza.views import home, ordering_pizza,multi_order
 
 urlpatterns = [
-    path('', home, name='home')
+    path("", home, name='home'),
+    path("order/", ordering_pizza, name='order'),
+    path("multiorder/", multi_order, name='order'),
 ]
